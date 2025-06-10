71a03a2d0cc53d8dd149b22b6313355c695e84f8
Prateek regmi <prateek.regmi@gmail.com>
2025-06-10 17:09:38 +1000

    Completed basic layout, and Css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,27 @@
-import * as React from "react";
-import { Link } from "gatsby";
+import * as React from "react"
+import { Link } from "gatsby"
+import { container,heading, navLinks, navLinkItem, navLinkText} from "./layout.module.css"
+console.log('container', container)
 
 const Layout = ({pageTitle, pageHeading, children}) => {
   return (
-    <main>  
+    <main className={container}>  
         <title>{pageTitle}</title>      
         <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className={navLinkItem}>
+            <Link to="/about"  className={navLinkText}>About</Link>
           </li>
-          <li>
-            <Link to="/contact">Contact</Link>
+          <li className={navLinkItem}>
+            <Link to="/contact"  className={navLinkText}>Contact</Link>
           </li>
         </ul>
         </nav>
 
-      <h1>{pageHeading}</h1>    
+      <h1 className={heading}>{pageHeading}</h1>    
       {children}
     </main>
   );
